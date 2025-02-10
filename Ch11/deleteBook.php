d5584fd49a0d26b40a3b259e3941c4b93c7745fe
Jexd223 <s6606021611803@email.kmutnb.ac.th>
2025-02-10 15:48:38 +0700

    sa

--- Ch11/deleteBook.php
+++ Ch11/deleteBook.php
@@ -3,13 +3,15 @@
     $hostname = "localhost";
     $username = "root";
     $password = "";
-    $dbName = "bookStore";
+    $dbName = "bookstores";
     $conn = mysqli_connect($hostname, $username, $password);
+
     if (!$conn)
         die("ไม่สามารถติดต่อกับ mySQL ได้");
+    
     mysqli_select_db($conn, $dbName) or die("ไม่สามารถเลือกฐานข้อมูล bookStore ได้");
     $sql = "delete from book where bookId='$bookId'";
-    mysqli_query($conn, $sql) or die ("delete จากตาราง book มีข้อผิดพลาดเกิดขึ้น".mysqli_error());
+    mysqli_query($conn, $sql) or die("delete จากตาราง book มีข้อผิดพลาดเกิดขึ้น" . mysqli_error($conn));
     mysqli_close($conn);
-    header("location:bookList1.php");
+    header("location:listBook.php");
 ?>
