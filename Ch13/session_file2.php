674c30fb2dd6575133d390b48d5e5e752bce3987
Jexd223 <s6606021611803@email.kmutnb.ac.th>
2025-02-17 14:19:53 +0700

    UPPPP

---
+++ Ch13/session_file2.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    $_SESSION['username']=$username;
+    $_SESSION['password']=$password;
+?>
+ชื่อผู้ใช้ที่คุณป้อนมาให้คือ
+<?php
+    echo $_SESSION['username'];
+?>
+<br><br>
+<a href="session_file3.php">คลิกตรงนี้เพื่อไปยังไฟล์ session_file3.php</a>
